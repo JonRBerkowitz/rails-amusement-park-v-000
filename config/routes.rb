d5512cf9fe7a1e08496dda2c878fa839fc3074f1
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-06-06 20:43:51 +0000

    started attraction functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
   resources :users, only: [:new, :create, :show]
+  get '/attractions', to: 'attractions#index'
+  resources :attractions
 end
